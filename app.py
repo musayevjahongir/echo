549e9a5e01ba5cba1a4f82b6c8f3932a3d53d638
musayevjahongir <musayevjahongir5051@gmail.com>
2024-01-30 16:29:32 +0500

    new2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,request
 import telegram
 # Create the application instance
-TOKEN ='6244092180:AAH_10MCMX4wAESk6TTp-iboI1EMSe6FeZ8'
+TOKEN ='6804364814:AAFG6WL6EcqnIgL_9qY4euWHUcvGaCKLU08'
 bot = telegram.Bot(TOKEN)
 # chat_id = '1432402481'
 app = Flask(__name__)
