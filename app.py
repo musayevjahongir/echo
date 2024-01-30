3766a9b932a31967c9ef13f4e0d5e240ef6bde93
musayevjahongir <musayevjahongir5051@gmail.com>
2024-01-30 16:23:20 +0500

    new1

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask,request
 import telegram
+# Create the application instance
+TOKEN ='6244092180:AAH_10MCMX4wAESk6TTp-iboI1EMSe6FeZ8'
+bot = telegram.Bot(TOKEN)
+# chat_id = '1432402481'
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello World!!'
-if __name__=='__main__':
+
+@app.route('/',methods=["POST"])
+def index():
+    data=request.get_json()
+    print(data)
+    bot.send_message(chat_id=data['message']['chat']['id'],text=data['message']['text'])
+    return 'index page'
+
+if __name__=="__main__":
     app.run(debug=True)
