348b0eebf5bfc25705ade1f3014f6e68dc789b48
musayevjahongir <musayevjahongir5051@gmail.com>
2024-01-31 21:14:39 +0500

    xato

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
     data=request.get_json()
     print(data)
     bot.send_message(chat_id=data['message']['chat']['id'],text=data['message']['text'])
-    return 'index page'
+    
 
 if __name__=="__main__":
     app.run(debug=True)
